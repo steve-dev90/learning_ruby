13356da47ba8287e30853b289adf76dc6fe0ee2e
Steve Torrens <stevetorrens@Steves-MacBook-Air.local>
2018-06-27 19:42:51 +1200

    First file

---
+++ widgets.rb
@@ -0,0 +1,17 @@
+#This is a comment
+#puts = put string method
+# Can enclose puts arguments in brackets
+puts "hello string"
+puts 1, 2, "a", "b"
+sleep 2
+print "cd"
+sleep 1
+print
+
+#Function names use lower case snake_case by convention
+def count
+    puts 1
+    puts 2
+end
+
+count
